18556d0e2a1780095c822fabd08bf2302c5739da
tonychg <antoine.chiny@gmail.com>
2019-01-19 17:04:43 +0100

    Header

--- splitline/splitline.h
+++ splitline/splitline.h
@@ -6,9 +6,16 @@
 #include <string.h>
 
 #define USAGE \
-    "           Usage: \n" \
-    "           ./splitline <string>\n" \
-    "           Output all the words from the string\n"
+    "                  _ _ _   _ _\n" \
+    "        ___ _ __ | (_) |_| (_)_ __   ___\n" \
+    "       / __| '_ \\| | | __| | | '_ \\ / _ \\\n" \
+    "       \\__ \\ |_) | | | |_| | | | | |  __/\n" \
+    "       |___/ .__/|_|_|\\__|_|_|_| |_|\\___|\n" \
+    "           |_|\n" \
+    "\n" \
+    " ++ Usage: \n" \
+    "    ./splitline <string> <pattern>\n" \
+    "    Output all the words from the string separate by <pattern>\n"
 
 // #define WHITE_SPACES "[ \t]+"
 #define WHITE_SPACES "[ \t]+"
